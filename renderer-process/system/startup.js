9fd10c8bc611d987e93bb7188c196f615a7a1001
hisune <hi@hisune.com>
2017-05-10 13:30:49 +0800

    增加程序启动器的分类

--- renderer-process/system/startup.js
+++ renderer-process/system/startup.js
@@ -5,7 +5,7 @@
 'use strict';
 
 const storage = require('electron-json-storage'),
-     clipboard = require('electron').clipboard;
+    clipboard = require('electron').clipboard;
 
 $('#add-startup,#add-startup-path').click(() => {
     const {dialog} = require('electron').remote,
@@ -42,6 +42,7 @@ $('#add-startup-save').click(() => {
         data[path] = {
             path: path,
             sort: $('#add-startup-sort').val(),
+            dir: $('#add-startup-dir').val(),
             icon: $('#add-startup-icon').val(),
             name: $('#add-startup-name').val(),
         }
@@ -56,23 +57,46 @@ var startupInit = function()
     $('#add-startup-action').hide();
     $('#add-startup').show();
     storage.get('startup', (error, data) => {
-        let array = [],
+        let obj = {},
             sc = $('#startup-container');
-        for(var i in data)
-            array.push(data[i]);
-        array.sort(function(a, b) {
-            return parseFloat(b.sort) - parseFloat(a.sort);
-        });
+
+        for(let i in data){
+            let dir = data[i].dir ? data[i].dir : '';
+            if(!obj.hasOwnProperty(dir)) obj[dir] = [];
+            obj[dir].push(data[i]);
+        }
+        let keys = Object.keys(obj),
+            len = keys.length;
+        keys.sort();
+
         sc.empty();
-        for(var j in array){
-            sc.append('<li onmouseover="$(this).children(\'span\').show()" onmouseleave="$(this).children(\'span\').hide()">' +
-                '<div class="startup-div" data-path="' + array[j].path + '" onclick="openStartup($(this))">' +
-                '<div class="glyphicon fa fa-' + array[j].icon + '" aria-hidden="true"></div>' +
-                '<div class="glyphicon-class" style="font-size: 16px;">' + array[j].name + '</div>' +
-                '</div>' +
-                '<span data-path="' + array[j].path + '" data-sort="' + array[j].sort + '" data-ico="' + array[j].icon +
-                '" data-name="' + array[j].name + '" class="startup-button-edit" onclick="displayEdit($(this))">编辑</span>' +
-                '</li>');
+
+        for (let i = 0; i < len; i++) {
+            let j = keys[i], container = '';
+            obj[j].sort(function(a, b) {
+                return parseFloat(b.sort) - parseFloat(a.sort);
+            });
+
+            if(j){
+                container += '<div style="clear: both;"></div><div class="demo">' +
+                    '<div class="demo-wrapper">' +
+                    '<button onclick="event.target.parentElement.classList.toggle(\'is-open\')" id="tray-demo-toggle" class="js-container-target demo-toggle-button">' + j + '</button>' +
+                    '<div class="demo-box demo-box2">';
+            }
+            for(let k in obj[j]){
+                container += '<li onmouseover="$(this).children(\'span\').show()" onmouseleave="$(this).children(\'span\').hide()">' +
+                    '<div class="startup-div" data-path="' + obj[j][k].path + '" onclick="openStartup($(this))">' +
+                    '<div class="glyphicon fa fa-' + obj[j][k].icon + '" aria-hidden="true"></div>' +
+                    '<div class="glyphicon-class" style="font-size: 16px;">' + obj[j][k].name + '</div>' +
+                    '</div>' +
+                    '<span data-path="' + obj[j][k].path + '" data-sort="' + obj[j][k].sort + '" data-dir="' + (obj[j][k].dir ? obj[j][k].dir : '') + '" data-ico="' + obj[j][k].icon +
+                    '" data-name="' + obj[j][k].name + '" class="startup-button-edit" onclick="displayEdit($(this))">编辑</span>' +
+                    '</li>';
+            }
+            if(j){
+                container += '</div></div></div>';
+            }
+            sc.append(container);
         }
     });
 }
